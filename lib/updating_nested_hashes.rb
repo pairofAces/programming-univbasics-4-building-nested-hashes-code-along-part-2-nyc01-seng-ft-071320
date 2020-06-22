32df8c76b908395760577039f88cc8a791d6f00c
Karan S. Chauhan <karanchauhan16@gmail.com>
2020-06-22 20:47:58 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -71,7 +71,7 @@ end
 
 def update_status
   epic_tragedy[:montague][:hero][:status] = "dead",
-  epic_tragedy[:capulet][:heroine][:status] = "dead",
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
   puts epic_tragedy
   
 end
